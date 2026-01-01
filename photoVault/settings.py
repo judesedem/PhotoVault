31d8fe3ee5c75301afb27306857a8ee2fb7a16c9
Jude Fiadzawoo <judefiadzawoo@gmail.com>
2026-01-01 22:15:44 +0000

    Started the throttling without fixing the earlier error

--- photoVault/settings.py
+++ photoVault/settings.py
@@ -165,3 +165,14 @@ CACHES = {
         }
     }
 }
+
+REST_FRAMEWORK = {
+    'DEFAULT_THROTTLE_CLASSES': [
+        'rest_framework.throttling.AnonRateThrottle',
+        'rest_framework.throttling.UserRateThrottle'
+    ],
+    'DEFAULT_THROTTLE_RATES': {
+        'anon': '100/day',
+        'user': '5/day'
+    }
+}
