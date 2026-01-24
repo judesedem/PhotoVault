ddad983c26f9a14022a8d4eb4200497a71fac56c
judesedem <judefiadzawoo@gmail.com>
2026-01-24 22:39:19 +0000

    rm comments

--- Vault/role_filters.py
+++ Vault/role_filters.py
@@ -10,11 +10,7 @@ class AdminRoleFilter(RoleFilter):
 class UserRoleFilter(RoleFilter):
     role_id = 'user'
     
-    def get_allowed_actions(self, request, view, obj=None):
-        # This example returns same list both for "global permissions" check,
-        # and for "object" permissions, but different list may be returned
-        # if `obj` argument is not None, and this list will be used to check
-        # if action is allowed during call to `ViewSet.check_object_permissions`
+    def get_allowed_actions(self, request, view, obj=None):        
         if request.method in SAFE_METHODS:
             return ['list', 'retrieve']
         return ['create', 'list', 'retrieve', 'update', 'partial_update']
